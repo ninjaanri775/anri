f0675c0363fae2fef44e53de17ecfee254d409cd
ninjaanri775 <jimsheleishvili775@gmail.com>
2024-06-28 16:55:32 +0400

    Delete static directory

--- static/items.js
+++
@@ -1,6 +0,0 @@
-const buy = document.querySelector(".buy")
-
-
-buy.addEventListener("click", () => {
-    alert("Purchase failed, Server got problems")
-})
--- static/products.js
+++
@@ -1,6 +0,0 @@
-const backbutton = document.querySelector(".backbutton")
-
-
-backbutton.addEventListener("click", () => {
-    alert("Purchase failed, Server got problems")
-})
